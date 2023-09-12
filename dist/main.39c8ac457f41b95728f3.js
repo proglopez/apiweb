d591792b9e7acc641415fcd47050e70042c46d8b
proglopez <prog.lopez@gmail.com>
2023-09-12 17:32:41 -0500

    get-post

---
+++ dist/main.39c8ac457f41b95728f3.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("// let items = [];\r\n\r\n// document.addEventListener(\"DOMContentLoaded\", () => {\r\n//     fetch(\"/data/data.json\")\r\n//         .then(response => response.json())\r\n//         .then(data => {\r\n//             items = data;\r\n//             displayItems();\r\n//         });\r\n\r\n//     document.getElementById(\"itemForm\").addEventListener(\"submit\", (event) => {\r\n//         event.preventDefault();\r\n//         const id = items.length ? items[items.length - 1].id + 1 : 1;\r\n//         const name = document.getElementById(\"itemName\").value;\r\n//         const description = document.getElementById(\"itemDescription\").value;\r\n\r\n//         items.push({ id, name, description });\r\n//         displayItems();\r\n\r\n//         document.getElementById(\"itemName\").value = \"\";\r\n//         document.getElementById(\"itemDescription\").value = \"\";\r\n//     });\r\n// });\r\n\r\n// function displayItems() {\r\n//     const itemList = document.getElementById(\"itemList\");\r\n//     itemList.innerHTML = \"\";\r\n//     items.forEach(item => {\r\n//         const li = document.createElement(\"li\");\r\n//         li.textContent = `${item.name}: ${item.description}`;\r\n//         li.appendChild(createDeleteButton(item.id));\r\n//         itemList.appendChild(li);\r\n//     });\r\n// }\r\n\r\n// function createDeleteButton(id) {\r\n//     const button = document.createElement(\"button\");\r\n//     button.textContent = \"Eliminar\";\r\n//     button.addEventListener(\"click\", () => {\r\n//         items = items.filter(item => item.id !== id);\r\n//         displayItems();\r\n//     });\r\n//     return button;\r\n// }\r\n\n\n//# sourceURL=webpack://apiweb/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
